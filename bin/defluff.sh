3ca3663109c410177c420de9595a2dd43f3277c1
Ranjit Mathew <rmathew@gmail.com>
2021-11-01 21:18:39 +0530

    Add a post about updating Linux for Rust.

--- bin/defluff.sh
+++ bin/defluff.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
-# Removes leading/trailing spaces/tabs and empty lines from a
-# given file or from the standard input.
-
-if [ -z "$1" ]
-then
+#!/usr/bin/env bash
+#
+# Removes leading/trailing spaces/tabs and empty lines from a given file or from
+# the standard input.
+#
+if [[ -z "${1}" ]]; then
   sed 's/^[ \t]*//;s/[ \t]*$//' | sed '/^$/d'
 else
-  sed -i 's/^[ \t]*//;s/[ \t]*$//' "$1"
-  sed -i '/^$/d' "$1"
+  sed -i 's/^[ \t]*//;s/[ \t]*$//' "${1}"
+  sed -i '/^$/d' "${1}"
 fi
--- bin/genminpgs.sh
+++ bin/genminpgs.sh
@@ -7,12 +7,8 @@
 #
 # <cat_id>, if specified, is the category-identifier for a book.
 
-set -o errexit
-set -o pipefail
-
-if [ -n "$1" ]
-then
-  PAGE_ID="$1"
+if [[ -n "${1}" ]]; then
+  PAGE_ID="${1}"
 else
   echo ERROR: Missing page-identifier.
   echo
@@ -22,54 +18,52 @@ else
   exit 1
 fi
 
-if [ -n "$2" ]
-then
-  BOOK_CAT="$2"
+BOOK_CAT=""
+if [[ -n "${2}" ]]; then
+  BOOK_CAT="${2}"
 fi
 
+set -euo pipefail
+
 CURR_YEAR=${CURR_YEAR:-$(date +%Y)}
-if [ ! -f "$PWD/src/$CURR_YEAR/$PAGE_ID.htm4" ]
+if [[ ! -f "${PWD}/src/${CURR_YEAR}/${PAGE_ID}.htm4" ]]
 then
-  echo ERROR: Missing page \"src/$CURR_YEAR/$PAGE_ID.htm4\".
+  echo ERROR: Missing page \"src/${CURR_YEAR}/${PAGE_ID}.htm4\".
   exit 1
 fi
 
-if [ -n "$BOOK_CAT" ]
-then
-  BOOK_IMG="$CURR_YEAR/$PAGE_ID.jpg"
-  if [ ! -f "$PWD/src/$BOOK_IMG" ]
-  then
-    echo ERROR: Missing book-cover image \"src/$BOOK_IMG\".
+if [[ -n "${BOOK_CAT}" ]]; then
+  BOOK_IMG="${CURR_YEAR}/${PAGE_ID}.jpg"
+  if [[ ! -f "${PWD}/src/${BOOK_IMG}" ]]; then
+    echo ERROR: Missing book-cover image \"src/${BOOK_IMG}\".
     exit 1
   fi
 fi
 
-if [ ! -f deps.mk ]
-then
+if [[ ! -f deps.mk ]]; then
   make deps
 fi
 
-UPDT_FILES="index.html atom.xml archives/index.html $CURR_YEAR/index.html"
-UPDT_FILES="$UPDT_FILES $CURR_YEAR/$PAGE_ID.html"
-if [ -n "$BOOK_CAT" ]
-then
-  UPDT_FILES="$UPDT_FILES books/$BOOK_CAT.html"
+UPDT_FILES="${CURR_YEAR}/${PAGE_ID}.html"
+UPDT_FILES="${UPDT_FILES} index.html atom.xml archives/index.html"
+UPDT_FILES="${UPDT_FILES} ${CURR_YEAR}/index.html"
+if [[ -n "${BOOK_CAT}" ]]; then
+  UPDT_FILES="${UPDT_FILES} books/${BOOK_CAT}.html"
 fi
 
-for A_FILE in $UPDT_FILES
+for A_FILE in ${UPDT_FILES}
 do
-  make $PWD/pub/$A_FILE
+  make "${PWD}/pub/${A_FILE}"
 done
 
-UPLOAD_FILES="sitemap.xml $UPDT_FILES"
-if [ -n "$BOOK_CAT" ]
-then
-  UPLOAD_FILES="$UPLOAD_FILES $BOOK_IMG"
+UPLOAD_FILES="sitemap.xml ${UPDT_FILES}"
+if [[ -n "${BOOK_CAT}" ]]; then
+  UPLOAD_FILES="${UPLOAD_FILES} ${BOOK_IMG}"
 fi
 
-echo
+echo ""
 echo "Files to upload:"
-for A_FILE in $UPLOAD_FILES
+for A_FILE in ${UPLOAD_FILES}
 do
-  echo "  \"$A_FILE\""
+  echo "  \"${A_FILE}\""
 done
